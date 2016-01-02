54ce240dfe004f0a08426654a9d77f2e6951257b
Jamie Macey <jamie.git@tracefunc.com>
2016-01-01 22:01:16 -0800

    render the board

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,4 +2,20 @@ class GameController < ApplicationController
   def index
     render
   end
+
+  def show
+    @map = [
+      [ nil       ,  nil         , :london     ,  nil         , :essen   ,  nil     ,  nil    ],
+      [:montreal  , :newyork     , :madrid     , :paris       , :milan   , :stpeters,  nil    ],
+      [:atlanta   , :washington  ,  nil        , :algiers     ,  nil     , :istanbul, :moscow ],
+      [ nil       ,  nil         , :lagos      ,  nil         , :cairo   ,  nil     ,  nil    ],
+      [:miami     , :saopaulo    , :kinshasa   , :khartoum    , :riyadh  , :baghdad ,  nil    ],
+      [ nil       , :bogota      , :buenosaires, :johannesburg, :mumbai  , :karachi , :tehran ],
+      [:mexicocity, :lima        , :santiago   , :chennai     ,  nil     , :delhi   ,  nil    ],
+      [ nil       , :losangeles  , :jakarta    , :bangkok     , :kolkata ,  nil     , :beijing],
+      [:chicago   ,  nil         , :sydney     , :hochiminh   , :hongkong, :shanghai, :seoul  ],
+      [ nil       , :sanfrancisco,  nil        , :manila      , :taipei  , :osaka   , :tokyo  ],
+    ]
+    render
+  end
 end
