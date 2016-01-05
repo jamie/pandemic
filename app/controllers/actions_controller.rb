fa48f532f696f16edadaefa1fb0a06a9083e65ce
Jamie Macey <jamie.git@tracefunc.com>
2016-01-04 19:56:59 -0800

    movin' mah medic

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,5 +1,7 @@
 class ActionsController < ApplicationController
   def create
+    city = City.find(params[:city])
+    Player.first.update_attributes(x: city.x, y: city.y)
     redirect_to :back
   end
 end
